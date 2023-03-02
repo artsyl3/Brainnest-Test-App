fcc6eec134fa7ebee737f97b369534e1dcf6c2dd
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-03-02 09:10:26 +0100

    added tests

--- src/analyzeArray.js
+++ src/analyzeArray.js
@@ -1,10 +1,14 @@
-function analyzeArray(arr) {
-  const sum = arr.reduce((acc, cur) => acc + cur, 0);
+const analyzeArray = (arr) => {
+  const sum = arr.reduce((a, b) => a + b,0);
   const average = sum / arr.length;
   const min = Math.min(...arr);
   const max = Math.max(...arr);
   const length = arr.length;
   return { average, min, max, length };
 }
+
+const obj = analyzeArray([-1,-2,-4,-5]);
+
+console.log(obj)
 
 module.exports = analyzeArray;
