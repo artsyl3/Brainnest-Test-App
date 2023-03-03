8c4beb4515421d4907d86dffc514609e964a8086
SnuggleTrouble <90455892+SnuggleTrouble@users.noreply.github.com>
2023-03-03 14:28:48 +0100

    attempt jest fix. unsuccessful

--- src/calculator.js
+++ src/calculator.js
@@ -1,51 +1,51 @@
 const calculator = {
-  add: function(num1, num2) {
+  add: function (num1, num2) {
     return num1 + num2;
   },
-  subtract: function(num1, num2) {
+  subtract: function (num1, num2) {
     return num1 - num2;
   },
-  multiply: function(num1, num2) {
+  multiply: function (num1, num2) {
     return num1 * num2;
   },
-  divide: function(num1, num2) {
+  divide: function (num1, num2) {
     if (num2 === 0) {
       throw new Error("Cannot divide by zero!");
     }
     return num1 / num2;
-  }
+  },
 };
 
-const addBtn = document.getElementById('add');
-const subtractBtn = document.getElementById('subtract');
-const multiplyBtn = document.getElementById('multiply');
-const divideBtn = document.getElementById('divide');
-const num1Input = document.getElementById('num1');
-const num2Input = document.getElementById('num2');
-const resultDiv = document.getElementById('result');
+const addBtn = document.getElementById("add");
+const subtractBtn = document.getElementById("subtract");
+const multiplyBtn = document.getElementById("multiply");
+const divideBtn = document.getElementById("divide");
+const num1Input = document.getElementById("num1");
+const num2Input = document.getElementById("num2");
+const resultDiv = document.getElementById("result");
 
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
   const num1 = Number(num1Input.value);
   const num2 = Number(num2Input.value);
   const result = calculator.add(num1, num2);
   resultDiv.textContent = result;
 });
 
-subtractBtn.addEventListener('click', () => {
+subtractBtn.addEventListener("click", () => {
   const num1 = Number(num1Input.value);
   const num2 = Number(num2Input.value);
   const result = calculator.subtract(num1, num2);
   resultDiv.textContent = result;
 });
 
-multiplyBtn.addEventListener('click', () => {
+multiplyBtn.addEventListener("click", () => {
   const num1 = Number(num1Input.value);
   const num2 = Number(num2Input.value);
   const result = calculator.multiply(num1, num2);
   resultDiv.textContent = result;
 });
 
-divideBtn.addEventListener('click', () => {
+divideBtn.addEventListener("click", () => {
   const num1 = Number(num1Input.value);
   const num2 = Number(num2Input.value);
   try {
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,83 @@
-const calculator = require("../src/calculator");
+/**
+ * @jest-environment jsdom
+ */
 
-describe("calculator object", () => {
-  test("add function adds two numbers", () => {
+const calculator = require("../src/calculator").default;
+
+// Create fake DOM environment for Jest
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>");
+global.document = dom.window.document;
+global.window = dom.window;
+
+describe("Calculator", () => {
+  test("add function should return the correct result", () => {
     expect(calculator.add(2, 3)).toBe(5);
-    expect(calculator.add(-2, 3)).toBe(1);
-    expect(calculator.add(0, 0)).toBe(0);
   });
-  test("subtract function subtracts two numbers", () => {
-    expect(calculator.subtract(2, 3)).toBe(-1);
-    expect(calculator.subtract(-2, 3)).toBe(-5);
-    expect(calculator.subtract(0, 0)).toBe(0);
+
+  test("subtract function should return the correct result", () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
   });
-  test("divide function divides two numbers", () => {
-    expect(calculator.divide(6, 3)).toBe(2);
-    expect(calculator.divide(-6, 3)).toBe(-2);
-    expect(calculator.divide(3, 0)).toBe(Infinity);
-  });
-  test("multiply function multiplies two numbers", () => {
+
+  test("multiply function should return the correct result", () => {
     expect(calculator.multiply(2, 3)).toBe(6);
-    expect(calculator.multiply(-2, 3)).toBe(-6);
-    expect(calculator.multiply(0, 0)).toBe(0);
+  });
+
+  test("divide function should return the correct result", () => {
+    expect(calculator.divide(6, 3)).toBe(2);
+  });
+
+  test("divide function should throw an error when dividing by zero", () => {
+    expect(() => {
+      calculator.divide(6, 0);
+    }).toThrowError("Cannot divide by zero!");
   });
 });
+
+/* describe("Testing if calculator function can manipulate the dom", () => {
+  test("Check add if it can get inputs and display results", () => {
+    document.body.innerText = `
+    <input id="num1" />
+    <input id="num2" />
+    <button id="add">Add</button>
+    <div id="result"></div>
+    `;
+    expect(calculator.add(2, 3)).toBe(5);
+  });
+});
+
+describe("Testing if calculator function can manipulate the dom", () => {
+  test("Check subtract if it can get inputs and display results", () => {
+    document.body.innerText = `
+    <input id="num1" />
+    <input id="num2" />
+    <button id="subtract">Subtract</button>
+    <div id="result"></div>
+    `;
+    expect(calculator.subtract(5, 3)).toBe(2);
+  });
+});
+describe("Testing if calculator function can manipulate the dom", () => {
+  test("Check multiply if it can get inputs and display results", () => {
+    document.body.innerText = `
+    <input id="num1" />
+    <input id="num2" />
+    <button id="multiply">Multiply</button>
+    <div id="result"></div>
+    `;
+    expect(calculator.multiply(2, 3)).toBe(6);
+  });
+});
+describe("Testing if calculator function can manipulate the dom", () => {
+  test("Check divide if it can get inputs and display results", () => {
+    document.body.innerText = `
+    <input id="num1" />
+    <input id="num2" />
+    <button id="divide">Divide</button>
+    <div id="result"></div>
+    `;
+    expect(calculator.divide(6, 3)).toBe(2);
+  });
+});
+ */
